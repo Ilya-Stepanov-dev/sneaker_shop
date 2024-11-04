24b0247812d43cebfc9852ac11054312d76a6d82
Ilya Stepanov <ilya.stepanov.dev@gmail.com>
2024-11-04 22:10:12 +0300

    configuration file changed

--- app/config.py
+++ app/config.py
@@ -3,10 +3,6 @@ from pydantic import BaseModel, field_validator
 from pydantic_settings import BaseSettings, SettingsConfigDict
 
 
-# DOTENV = os.path.join(os.path.dirname(__file__), ".env")
-# DOTENV_PROD = os.path.join(os.path.dirname(__file__), ".env-prod")
-
-
 class UvicornConfig(BaseModel):
     host: str = 'host'
     port: int = 0000
@@ -43,18 +39,9 @@ class Settings(BaseSettings):
         case_sensitive=False,
         env_nested_delimiter="__",
         env_prefix="APP_CONFIG__",
-        # env_file_encoding="utf-8",
-        # extra='allow'
     )
     uvicorn: UvicornConfig = UvicornConfig()
     api: ApiConfig = ApiConfig()
     db: DataBaseConfig = DataBaseConfig()
 
-
-
-# uvicorn = UvicornConfig()
-
-# print(uvicorn)
-
 settings = Settings()
-print(settings.db.get_url())
