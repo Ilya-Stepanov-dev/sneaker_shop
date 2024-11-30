0583f351e87917fb8aeab4bd2ea464481dc5d41f
Ilya Stepanov <ilya.stepanov.dev@gmail.com>
2024-11-30 19:46:57 +0300

    renamed module core_schemas (one letter was written chiral)

---
+++ app/core_schemas/base.py
@@ -0,0 +1,9 @@
+# from pydantic import BaseModel
+from datetime import datetime
+from sqlalchemy.orm import Mapped, mapped_column
+from sqlalchemy import DateTime, func
+
+class BaseClass():
+
+    date_creation: Mapped[datetime] = mapped_column(DateTime, server_default=func.now())
+    date_update: Mapped[datetime] = mapped_column(DateTime, onupdate=func.now())
---
+++ app/core_schemas/user.py
@@ -0,0 +1,22 @@
+from typing import Annotated
+# from annotated_types import Gt, Le, MaxLen, Date
+from pydantic import BaseModel, Field
+from datetime import datetime
+from .base import BaseClass
+
+class TelegramUser(BaseModel,BaseClass):
+
+    id: Annotated[int , Field(gt=0)]
+    user_name: Annotated[str, Field(min_length=2, max_length=50)]
+
+class User(BaseModel, BaseClass):
+
+    id: Annotated[int , Field(gt=0)]
+    first_name: Annotated[str, Field(min_length=2, max_length=50)]
+    last_name: Annotated[str, Field(min_length=2, max_length=50)]
+    phone: str
+    email: str
+    birthday: datetime
+
+    # TODO: дописать валидацию для номера и email
+    # TODO: посмотреть порядок наследования
